6469c509d444b08ce7c1912e113cef01745d7474
Lyndon Panton <LyndonPanton@gmail.com>
2024-04-15 22:08:39 +0100

    Defined the JEasyFrame class

---
+++ src/utilities/JEasyFrame.java
@@ -0,0 +1,19 @@
+package utilities;
+
+import javax.swing.JFrame;
+import java.awt.Component;
+import java.awt.BorderLayout;
+
+public class JEasyFrame extends JFrame {
+    public Component component;
+
+    public JEasyFrame(Component component, String title) {
+        super(title);
+        this.component = component;
+        getContentPane().add(BorderLayout.CENTER, component);
+        pack();
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        repaint();
+    }
+}
