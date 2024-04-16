c9e1ab6ca540f3c639a51318682c4b1049d32559
Lyndon Panton <LyndonPanton@gmail.com>
2024-04-16 21:48:34 +0100

    Extended the constants class

--- src/utilities/Constants.java
+++ src/utilities/Constants.java
@@ -7,4 +7,6 @@ public class Constants {
     public static final int FRAME_HEIGHT = 480;
     public static final Dimension FRAME_SIZE =
             new Dimension(FRAME_WIDTH, FRAME_HEIGHT);
+    public static final int DELAY = 20;
+    public static final double DT = DELAY / 1000.0;
 }
